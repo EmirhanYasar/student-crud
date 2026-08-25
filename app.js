be5b2ade7268eba442c9eabf0fbf641d931201d0
Emirhan-web <emirhan.yasar@istun.edu.tr>
2026-08-25 12:49:09 +0300

    ekleme ,silme işlemleri denendi

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const pool = require("./db");
 
 async function testConnection() {
     try {
-        const result = await pool.query("SELECT NOW()");
-        console.log("Veritabanı bağlantısı başarılı!");
-        console.log(result.rows[0]);
+        const result = await pool.query(" DELETE FROM students WHERE id = $1 RETURNING *",
+            [9]
+        );
+        console.log(result.rows);
+        
     } catch (error) {
         console.error("Veritabanı bağlantı hatası:", error.message);
     } finally {
