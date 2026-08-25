81709fd1321b6e2a76286e0da531168611df0c4a
Emirhan-web <emirhan.yasar@istun.edu.tr>
2026-08-25 13:46:59 +0300

    Öğrenci yönetim sistemi için kullanıcı arayüzü ve fonksiyonlar eklendi

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 const pool = require("./db");
+const readline = require("readline");
+
+
+
+
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+
+});
+
+function askQuestion(question){
+    return new Promise((resolve)=> {
+        r1.question(question,(answer ) =>{
+            resolve(answer);
+        })
+    })
+}
+
+
 
 async function getStudents(){
     const result = await pool.query(
@@ -10,7 +30,7 @@ async function getStudents(){
 
 async function addStudent(name,age,email){
     const result = await pool.query(
-        "INSERT INTO students ( name,age,email) VALUES ($1,$2,$3) RETURNING",
+        "INSERT INTO students ( name,age,email) VALUES ($1,$2,$3) RETURNING* ",
         [name,age,email]
     );
 
@@ -35,3 +55,58 @@ async function deleteStudent(id){
 
     console.log(result.rows);
 }
+
+
+async function main() {
+
+    try{
+        console.log("\n--- Öğrenci Yönetim Sistemi ---");
+        console.log("1 - Öğrenci Ekle");
+        console.log("2 - Öğrencileri Listele");
+        console.log("3 - Öğrenci Güncelle");
+        console.log("4 - Öğrenci Sil");
+        console.log("5 - Çıkış");
+
+        const choice = await askQuestion("Seçiminiz: ");
+        
+        if(choice === "1"){
+            const name = await askQuestion("Ad: ");
+            const age = Number(await askQuestion("Yaşı: "));
+            const email = await askQuestion("Email: ");
+
+            await addStudent(name,age,email);
+        }
+
+        else if(choice === "2"){
+            await getStudents();
+        }
+
+        else if(choice === "3"){
+            const id = Number(await askQuestion("Güncellenecek Öğrencinin ID'si: "));
+            const age = Number(await askQuestion("Yeni yaş: "));
+
+            await updateStudent(id,yaş);
+        }
+
+        else if(choice === "4"){
+            const id = Number(await askQuestion("Silinecek Öğrencinin ID'si: "));
+            await deleteStudent(id);
+        }
+
+        else if (choice === "5"){
+            console.log("Geçersiz seçim!");
+        }
+        
+
+    } catch(error){
+        console.error("Hata: ", error.message);
+
+    } finally{
+        r1.close();
+        await pool.end();
+    }
+}
+
+main();
+
+
