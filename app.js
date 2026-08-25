217aad9c5f3c9cf4a7958e386441ffe4af7d171d
Emirhan-web <emirhan.yasar@istun.edu.tr>
2026-08-25 12:57:37 +0300

    Veritabanından öğrenci getirme ve veritabanına öğrenci ekleme işlevleri eklendi.

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 const pool = require("./db");
 
+async function getStudents(){
+    const result = await pool.query(
+        "SELECT * FROM students"
+    );
+    console.log(result.rows)
+
+}
+
+async function addStudent(name,age,email){
+    const result = await pool.query(
+        "INSERT INTO students ( name,age,email) VALUES ($1,$2,$3) RETURNING",
+        [name,age,email]
+    );
+
+}
+
+
+
+
+
 async function testConnection() {
     try {
         const result = await pool.query(" DELETE FROM students WHERE id = $1 RETURNING *",
