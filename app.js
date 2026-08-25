acffb5f0e6e785c637c4397f4b8a3ce1f792084d
Emirhan-web <emirhan.yasar@istun.edu.tr>
2026-08-25 12:11:52 +0300

    Veritabanı bağlantı kurulumunu ve bağlantı test edildi

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Student CRUD Uygulaması Başladı");
+const pool = require("./db");
+
+async function testConnection() {
+    try {
+        const result = await pool.query("SELECT NOW()");
+        console.log("Veritabanı bağlantısı başarılı!");
+        console.log(result.rows[0]);
+    } catch (error) {
+        console.error("Veritabanı bağlantı hatası:", error.message);
+    } finally {
+        await pool.end();
+    }
+}
+
+testConnection();
